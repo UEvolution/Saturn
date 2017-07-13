e0853e9e4aa0bff0032103e8ba090e19aff56175
wolyshaw <shaw@xwlong.com>
2017-07-13 21:08:21 +0800

    belongsTo

--- routers/front/article.js
+++ routers/front/article.js
@@ -2,17 +2,21 @@ const express = require('express')
 const router = express.Router()
 const { sendSuccess, sendError } = require('../../util/util')
 const articles = require('../../models/articles')
+const users = require('../../models/users')
+
+const articlesUsers = articles.belongsTo(users, {foreignKey: 'author'})
 
 router.post('/list', (req, res) => {
   let { offset = 0, limit = 10 }  = req.body.page || {}
-  articles.findAndCountAll({ offset, limit })
+  articles.findAndCountAll({ offset, limit, include: [articlesUsers] })
     .then(r => res.json(sendSuccess(r)))
     .catch(error => res.json(sendError(undefined, error)))
 })
 
 router.post('/view', (req, res) => {
   articles.findOne({
-    where: req.body
+    where: req.body,
+    include: [articlesUsers]
   })
     .then(r => res.json(sendSuccess(r)))
     .catch(error => res.json(sendError(undefined, error)))
@@ -20,7 +24,7 @@ router.post('/view', (req, res) => {
 
 router.post('/edit', (req, res) => {
   let { id }  = req.body
-  articles.update(req.body, {where: {id}})
+  articles.update(req.body, {where: {id}, include: [articlesUsers]})
     .then(r => res.json(sendSuccess(r)))
     .catch(error => res.json(sendError(undefined, error)))
 })
