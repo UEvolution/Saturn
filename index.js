cecaf22feb582f7d43b8b4c933aa0d988969d56b
wolyshaw <wolyshaw@users.noreply.github.com>
2017-09-17 20:31:01 +0800

    res headers

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ app.use((err, req, res, next) => res.status(500).json(sendServerError))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers')
   next()
 })
 
