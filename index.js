03cca494c249cf31f4acb2bafbc9e41499f00637
wolyshaw <wolyshaw@users.noreply.github.com>
2017-09-17 20:34:28 +0800

    origin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => logger.info(`api ${req.method} router: ${req.url}`) 
 app.use((err, req, res, next) => res.status(500).json(sendServerError))
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', 'localhost,www.abcdea.com')
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers')
