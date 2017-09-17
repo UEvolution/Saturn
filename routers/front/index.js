8e83db75730d81d510b2d9bcaf38aec361d9fdb8
wolyshaw <wolyshaw@users.noreply.github.com>
2017-09-17 15:26:32 +0800

    add access

--- routers/front/index.js
+++ routers/front/index.js
@@ -4,6 +4,11 @@ const article = require('./article')
 const user = require('./user')
 const tag = require('./tag')
 
+router.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  next()
+})
+
 router.use('/article', article)
 router.use('/user', user)
 router.use('/tag', tag)
